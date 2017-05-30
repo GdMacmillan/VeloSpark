3bdf8cb4c33c9ca30a8c375c9b3b67433a0c10ea
Gordon MacMillan <gmacilla@ymail.com>
2017-05-30 16:54:58 -0600

    wsgi.py had heroku_starter_app reference

--- velosparkapp/velosparkapp/wsgi.py
+++ velosparkapp/velosparkapp/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "velosparkapp.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "velosparkapp.velosparkapp.settings")
 
 application = get_wsgi_application()
